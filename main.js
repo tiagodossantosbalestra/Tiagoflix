1e59d69f6f3ed7858b998bcf2941fa8d985b6149
Tiago <tiago.amancio@escola.pr.gov.br>
2025-07-04 11:08:45 -0300

    4

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function inserirNome (){
-    let nomeUsuario0=prompt("qual seu nome?");
-    let elemento=document.querySelector("#nome-usuario");
+    let nomeUsuario =prompt("qual seu nome?");
+    let elemento = document.querySelector("#nome-usuario");
     elemento.textcontent=nomeUsuario;
 }
 
